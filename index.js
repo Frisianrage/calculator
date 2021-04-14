c12bc959eda318071886d805f5703964f53bb703
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2021-04-14 16:45:54 -0700

    adding comments to the js file

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let result = 0
 
 resultScreen.innerHTML = 0
 
+//This updates screen of the calculator
 const screenUpdate = (number) => { 
     resultScreen.innerHTML = number
 }
 
+//This function does the actual math
 const operations = (num1, num2, oper) => {
     switch(oper) {
         case "+":
@@ -29,12 +31,14 @@ const operations = (num1, num2, oper) => {
     }
 }
 
+//Handles the input
 const input = (e) => {
     let btn = e.target
     const number = btn.innerHTML
 
     if(btn.classList.contains("operator")){
         operator = btn.innerHTML
+        //next line is needed if you want to use the result for the next calculation
         result == 0 ? storedNumber = parseInt(inputNumber) : storedNumber = result
         inputNumber = ""
         screenUpdate(0)
@@ -44,21 +48,26 @@ const input = (e) => {
     }
 }
 
+//Gives the buttons divided by equal-button, clear-button and every other button it's functionality
 const selector = (e) => {
     const btn = e.target
 
     if(btn.classList.contains("equal")) {
+        //clicking the equals button
         operations(storedNumber, parseInt(inputNumber), operator)
         screenUpdate(result)
     } else if(btn.classList.contains("clear")) {
+        //clicking the clear-button
         inputNumber = ""
         storedNumber = ""
         result = 0
         screenUpdate(0)
     } else {
+        //every other button
         input(e)
     }   
 }
 
+//Eventlistener for the whole button area
 const buttonArea = document.getElementById("button-area")
 buttonArea.addEventListener('click', selector)
